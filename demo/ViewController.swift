da1f9217d1ab8ee95973e2cdb6b7a71f51e4645a
NguyenHuuDung90 <tb.dungnh@gmail.com>
2017-08-02 10:24:23 +0700

    commit 10:24 2-8-2017

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -13,20 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let list2 = [ 10, 0, 3, 9, 2, 14, 26, 27, 1, 5, 8, -1, 8 ]
-//        partitionLomuto(&list2, low: 0, high: list2.count - 1)
-   
-//        quicksortLomuto(&list2, low: 0, high: list2.count - 1)
-//             print(list2)
-        
-//        let list1 = [ 10, 0, 3, 9, 2, 14, 8, 27, 1, 5, 8, -1, 26 ]
-//        print(quicksort(a: list1))
-//        print(quickSortAuto(a: list1))
-        print(quickSortAuto(a: list2))
+        let list = [ 0,1,4,8,2,6,4,9,10,4,1,2 ]
+
+        print(quickSort1(a: list))
+
+        print(quickSort2(a: list))
 
     }
     
-    func quicksort(a: [Int]) -> [Int] {
+    func quickSort1(a: [Int]) -> [Int] {
         guard a.count > 1 else { return a }
         
         let pivot = a[a.count/2]
@@ -34,15 +29,14 @@ class ViewController: UIViewController {
         let equal = a.filter { $0 == pivot }
         let greater = a.filter { $0 > pivot }
         
-        // Uncomment this following line to see in detail what the
-        // pivot is in each step and how the subarrays are partitioned.
-        //print(pivot, less, equal, greater)  return quicksort(less) + equal + quicksort(greater)
+    
         
-        return quicksort(a: less) + equal + quicksort(a: greater)
+        return quickSort1(a: less) + equal + quickSort1(a: greater)
     }
    
     
-    func quickSortAuto(a:[Int]) -> [Int] {
+    
+    func quickSort2(a:[Int]) -> [Int] {
         guard a.count > 1  else {
             return a
         }
@@ -72,38 +66,10 @@ class ViewController: UIViewController {
             }
         }
       
-        return quicksort(a:less) + equal + quicksort(a: greater)
-        
-        
+        return quickSort2(a:less) + equal + quickSort2(a: greater)
+    
     }
 
-    
-//    func partitionLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) -> Int {
-//        let pivot = a[high]
-//        
-//        var i = low
-//        for j in low..<high {
-//            if a[j] <= pivot {
-//                (a[i], a[j]) = (a[j], a[i])
-//                i += 1
-//            }
-//        }
-//        
-//        (a[i], a[high]) = (a[high], a[i])
-//        return i
-//    }
-//    
-//   
-//    
-//    func quicksortLomuto<T: Comparable>(_ a: inout [T], low: Int, high: Int) {
-//        if low < high {
-//            let p = partitionLomuto(&a, low: low, high: high)
-//            quicksortLomuto(&a, low: low, high: p - 1)
-//            quicksortLomuto(&a, low: p + 1, high: high)
-//        }
-//    }
-//    
- 
 
 }
 
